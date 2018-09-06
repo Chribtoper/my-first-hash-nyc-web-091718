4c820d56c57e16d9cbd2d60f3d514aec7cfc801b
Chribtoper <xcstolfax@gmail.com>
2018-09-06 20:16:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash(hash)
-  my_hash = hash
+    my_hash = {"name" => "Alexis", "age" => 26, "location" => "LA"}
+
 end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
